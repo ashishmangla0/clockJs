900a0abb9cd0d6f98da040cd340c8f290761486a
ashish <ashishmangla1991@gmail.com>
2020-12-20 17:33:56 +0530

    add clock in console

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 const blockHero = document.querySelectorAll(".block--hero");
+const getCurrentTime = () => {
+    let date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    console.log(date);
+    console.log(`i am hour : ${hr}`);
+    console.log(`i am Min : ${min}`);
+    console.log(`i am Sec : ${sec}`);
+  };
+  setInterval(getCurrentTime,1000);
